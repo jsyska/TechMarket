bfd725d53e49f3e837608722dc03b06f994ab75b
Jakub Syska <jaksyska@gmail.com>
2021-02-04 16:26:56 +0100

    fixed problems with bag button

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const productsDOM = document.querySelector('.products-center');
 
 //cart
 let cart = [];
+//buttons
+let buttonsDOM = [];
 
 //geting the products
 class Products{
@@ -74,9 +76,32 @@ class UI{
                 button.addEventListener("click", (event) =>{
                     event.target.innerText = "In Cart";
                     event.target.disabled = true;
+                    //get product from products
+                    let cartItem = {...Storage.getProduct(id), amount:1};
+                    
+                    //add product to the cart
+                    cart = [...cart,cartItem];
+
+                    //save the cart in local storage
+                    Storage.saveCart(cart)
+
+                    //set cart values
+                    this.setCartValues(cart);
+                    //display cart items
+                    //show the cart 
                 })
             }
         });
+    }
+    setCartValues(cart){
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
     }
 }
 
@@ -84,6 +109,13 @@ class UI{
 class Storage{
     static saveProducts(products){
         localStorage.setItem("products",JSON.stringify(products));
+    }
+    static getProduct(id){
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+    static saveCart(cart){
+        localStorage.setItem("cart",JSON.stringify(cart));
     }
 }
 
